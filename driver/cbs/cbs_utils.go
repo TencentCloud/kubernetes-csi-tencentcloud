fa335288a45a5b8da8a729394cd4a4df770f18d4
zichentian <zichentian@tencent.com>
2019-04-22 10:45:30 +0800

    Add snapshots func in controller-plugin

---
+++ driver/cbs/cbs_utils.go
@@ -0,0 +1,47 @@
+package cbs
+
+import (
+	"fmt"
+	"sync"
+)
+
+type cbsSnapshot struct {
+	SourceVolumeId     string `json:"sourceVolumeID"`
+	SnapName           string `json:"snapName"`
+	SnapId             string `json:"sanpID"`
+	CreatedAt          int64  `json:"createdAt"`
+	SizeBytes          int64  `json:"sizeBytes"`
+}
+
+type cbsSnapshotsCache struct {
+	mux      *sync.Mutex
+	cbsSnapshotMaps map[string]*cbsSnapshot
+}
+
+func (cache *cbsSnapshotsCache) add(id string, cbsSnap *cbsSnapshot){
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+
+	cache.cbsSnapshotMaps[id] = cbsSnap
+}
+
+func (cache *cbsSnapshotsCache) delete(id string){
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+
+	delete(cache.cbsSnapshotMaps, id)
+}
+
+func getCbsSnapshotByName(snapName string) (*cbsSnapshot, error) {
+	cbsSnapshotsMapsCache.mux.Lock()
+	defer cbsSnapshotsMapsCache.mux.Unlock()
+
+	for _, cbsSnap := range cbsSnapshotsMapsCache.cbsSnapshotMaps {
+		if cbsSnap.SnapName == snapName {
+			return cbsSnap, nil
+		}
+	}
+	return nil, fmt.Errorf("snapshot name %s does not exit in the snapshots list", snapName)
+}
+
+
