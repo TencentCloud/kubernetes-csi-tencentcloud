ab427210f4cb3f4c27139f16f962660ebee4b35b
marcriverli <marcriverli@tencent.com>
2019-12-18 17:34:23 +0800

    add cosfs csi

---
+++ build/cosfs/launcher.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
+
+	"k8s.io/kubernetes/pkg/util/mount"
+)
+
+const (
+	perm = 0600
+
+	SocketPath = "/tmp/cosfs.sock"
+)
+
+func main() {
+	flag.Parse()
+
+	prepareSocketDir()
+
+	r := mux.NewRouter()
+	launcher := r.Path("/launcher").Subrouter()
+	launcher.Methods("POST").HandlerFunc(launcherHandler)
+
+	mount := r.Path("/mount").Subrouter()
+	mount.Methods("POST").HandlerFunc(mountHandler)
+
+	server := http.Server{
+		Handler: r,
+	}
+
+	unixListener, err := net.Listen("unix", SocketPath)
+	if err != nil {
+		glog.Error(err)
+		return
+	}
+
+	if err := server.Serve(unixListener); err != nil {
+		glog.Errorf("cosfs launcher server closed unexpected. %v", err)
+	}
+
+	glog.Infoln("launcher server is running.")
+	return
+}
+
+func prepareSocketDir() {
+	if !isFileExisted(SocketPath) {
+		pathDir := filepath.Dir(SocketPath)
+		if !isFileExisted(pathDir) {
+			os.MkdirAll(pathDir, os.ModePerm)
+		}
+	} else {
+		os.Remove(SocketPath)
+	}
+
+	glog.Infof("socket dir %s is ready\n", filepath.Dir(SocketPath))
+}
+
+func launcherHandler(w http.ResponseWriter, r *http.Request) {
+	glog.Infoln("enter launcherHandler...")
+
+	extraFields := make(map[string]string)
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		extraFields["errmsg"] = "read request body failed"
+		generateHttpResponse(w, "failure", http.StatusInternalServerError, extraFields)
+		return
+	}
+
+	var bodyMap map[string]string
+	if err := json.Unmarshal(body, &bodyMap); err != nil {
+		extraFields["errmsg"] = "unmarshal request body failed"
+		generateHttpResponse(w, "failure", http.StatusInternalServerError, extraFields)
+		return
+	}
+
+	cmd, ok := bodyMap["command"]
+	if !ok {
+		extraFields["errmsg"] = "request body is empty. we need field `command`"
+		generateHttpResponse(w, "failure", http.StatusBadRequest, extraFields)
+		return
+	}
+
+	if err := execCmd(cmd); err != nil {
+		extraFields["errmsg"] = fmt.Sprintf("exec command(%s) failed. %v", cmd, err)
+		generateHttpResponse(w, "failure", http.StatusInternalServerError, extraFields)
+		return
+	}
+	glog.Infof("exec command %s success.\n", cmd)
+
+	generateHttpResponse(w, "success", http.StatusOK, extraFields)
+}
+
+func mountHandler(w http.ResponseWriter, r *http.Request) {
+	glog.Infoln("enter mountHandler...")
+	extraFields := make(map[string]string)
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		extraFields["errmsg"] = "read request body failed"
+		generateHttpResponse(w, "fail", http.StatusInternalServerError, extraFields)
+		return
+	}
+
+	var bodyMap map[string]string
+	if err := json.Unmarshal(body, &bodyMap); err != nil {
+		extraFields["errmsg"] = "unmarshal request body failed"
+		generateHttpResponse(w, "fail", http.StatusInternalServerError, extraFields)
+		return
+	}
+
+	stagingPath, ok := bodyMap["stagingPath"]
+	if !ok {
+		extraFields["errmsg"] = "request body is empty. we need field `staingPath`"
+		generateHttpResponse(w, "fail", http.StatusBadRequest, extraFields)
+		return
+	}
+
+	if !isFileExisted(stagingPath) {
+		glog.Infof("staging mount point path %s is not exist.\n", stagingPath)
+		if err := createMountPoint(stagingPath); err != nil {
+			glog.Errorf("failed to create staging mount point at %s: %v", stagingPath, err)
+			extraFields["errmsg"] = fmt.Sprintf("create staging mount point failed: %s", err)
+			generateHttpResponse(w, "fail", http.StatusInternalServerError, extraFields)
+			return
+		}
+	}
+
+	isMounted, err := isMountPoint(stagingPath)
+	if err != nil {
+		extraFields["errmsg"] = fmt.Sprintf("failed to check whether staging point mounted or not: %v", err)
+		generateHttpResponse(w, "fail", http.StatusInternalServerError, extraFields)
+		return
+	}
+
+	if isMounted {
+		glog.Infof("the staging path(%s) is a mount point\n", stagingPath)
+	} else {
+		glog.Infof("the staging path(%s) is not a mount point\n", stagingPath)
+	}
+
+	extraFields["isMounted"] = strconv.FormatBool(isMounted)
+	generateHttpResponse(w, "success", http.StatusOK, extraFields)
+}
+
+func isFileExisted(filename string) bool {
+	_, err := os.Stat(filename)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func execCmd(cmd string) error {
+	output, err := exec.Command("sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("command %s failed: output %s, error: %v", cmd, string(output), err)
+	}
+	glog.V(4).Infof("command %s succeed: %s", cmd, string(output))
+	return nil
+}
+
+func generateHttpResponse(w http.ResponseWriter, result string, statusCode int, extra map[string]string) {
+	res := make(map[string]string)
+	res["result"] = result
+	for k, v := range extra {
+		res[k] = v
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+func createMountPoint(point string) error {
+	return os.MkdirAll(point, perm)
+}
+
+func isMountPoint(path string) (bool, error) {
+	mounter := mount.New("")
+	notMnt, err := mounter.IsLikelyNotMountPoint(path)
+	if err != nil {
+		return false, err
+	}
+	return !notMnt, nil
+}
---
+++ driver/cosfs/nodeserver.go
@@ -0,0 +1,257 @@
+/*
+ Copyright 2019 THL A29 Limited, a Tencent company.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package cos
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/golang/glog"
+	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	// Used to create staging mount point password files.
+	perm = 0600
+
+	defaultDBGLevel          = "info"
+	cosPasswordFile          = "/etc/passwd-cosfs"
+	cosPasswordFileDirectory = "/tmp/"
+	secretKey                = "sec"
+	credentialID             = "SecretId"
+	credentialKey            = "SecretKey"
+	SocketPath               = "/tmp/cosfs.sock"
+)
+
+func newNodeServer(driver *csicommon.CSIDriver, mounter mounter) csi.NodeServer {
+	return &nodeServer{
+		DefaultNodeServer: csicommon.NewDefaultNodeServer(driver),
+		mounter:           mounter,
+	}
+}
+
+type nodeServer struct {
+	*csicommon.DefaultNodeServer
+	mounter mounter
+}
+
+type cosfsOptions struct {
+	URL            string
+	Bucket         string
+	DebugLevel     string
+	AdditionalArgs string
+}
+
+func (ns *nodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	if err := validateNodePublishVolumeRequest(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	volID := req.GetVolumeId()
+	targetPath := req.GetTargetPath()
+
+	options, err := parseCosfsOptions(req.GetVolumeContext())
+	if err != nil {
+		glog.Errorf("parse options from VolumeAttributes for %s failed: %v", volID, err)
+		return nil, status.Errorf(codes.InvalidArgument, "parse options failed: %v", err)
+	}
+
+	isMnt, err := ns.createMountPoint(volID, targetPath)
+	if err != nil {
+		return nil, err
+	}
+	if isMnt {
+		glog.Infof("Volume %s is already mounted to %s, skipping", volID, targetPath)
+		return &csi.NodePublishVolumeResponse{}, nil
+	}
+
+	// Extract the tmp credential info from NodeStageSecrets and store to a unique tmp file.
+	credFilePath, err := ns.createCredentialFile(volID, options.Bucket, req.GetSecrets())
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ns.mounter.Mount(options, targetPath, credFilePath); err != nil {
+		glog.Errorf("Mount %s to %s failed: %v", volID, targetPath, err)
+		return nil, status.Errorf(codes.Internal, "mount failed: %v", err)
+	}
+
+	glog.Infof("successfully mounted volume %s to %s", volID, targetPath)
+
+	return &csi.NodePublishVolumeResponse{}, nil
+}
+
+func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	if err := validateNodeUnpublishVolumeRequest(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	volID := req.GetVolumeId()
+	targetPath := req.GetTargetPath()
+
+	// use launcher
+	if err := ns.mounter.Umount(targetPath); err != nil {
+		glog.Errorf("Failed to umount point %s for volume %s: %v", targetPath, volID, err)
+		return nil, status.Errorf(codes.Internal, "umount failed: %v", err)
+	}
+
+	glog.Infof("Successfully unmounted volume %s from %s", req.GetVolumeId(), targetPath)
+
+	return &csi.NodeUnpublishVolumeResponse{}, nil
+}
+
+func (ns *nodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (ns *nodeServer) createMountPoint(volID, targetPath string) (bool, error) {
+	glog.Infof("Creating staging mount point at %s for volume %s", targetPath, volID)
+	return launcherCreateMountPoint(targetPath)
+}
+
+func parseCosfsOptions(attributes map[string]string) (*cosfsOptions, error) {
+	options := &cosfsOptions{DebugLevel: defaultDBGLevel}
+	for k, v := range attributes {
+		switch strings.ToLower(k) {
+		case "url":
+			options.URL = v
+		case "bucket":
+			options.Bucket = v
+		case "dbglevel":
+			options.DebugLevel = v
+		case "additional_args":
+			options.AdditionalArgs = v
+		}
+	}
+	return options, validateCosfsOptions(options)
+}
+
+func validateCosfsOptions(options *cosfsOptions) error {
+	if options.URL == "" {
+		return errors.New("COS service URL can't be empty")
+	}
+	if options.Bucket == "" {
+		return errors.New("COS bucket can't be empty")
+	}
+	return nil
+}
+
+func (ns *nodeServer) createCredentialFile(volID, bucket string, secrets map[string]string) (string, error) {
+	credential, err := getSecretCredential(bucket, secrets)
+	if err != nil {
+		glog.Errorf("getSecretCredential info from NodeStageSecrets failed: %v", err)
+		return "", status.Errorf(codes.InvalidArgument, "get credential failed: %v", err)
+	}
+
+	// compute password file sha256 and write is on password file name, so if file exist,
+	// then we needn't create a new password file
+	// file name like  testcos-123123123_fa51046944be10ef2d231dce44b3278414698678f9be0551a9299b15f75fecf1
+	credSHA := sha256.New()
+	credSHA.Write([]byte(credential))
+	shaString := string(hex.EncodeToString(credSHA.Sum(nil)))
+	passwdFilename := fmt.Sprintf("%s%s_%s", cosPasswordFileDirectory, bucket, shaString)
+
+	glog.Infof("cosfs password file name is %s", passwdFilename)
+
+	if _, err := os.Stat(passwdFilename); err != nil {
+		if os.IsNotExist(err) {
+			if err := ioutil.WriteFile(passwdFilename, []byte(credential), perm); err != nil {
+				glog.Errorf("create password file for volume %s failed: %v", volID, err)
+				return "", status.Errorf(codes.Internal, "create tmp password file failed: %v", err)
+			}
+		} else {
+			glog.Errorf("stat password file  %s failed: %v", passwdFilename, err)
+			return "", status.Errorf(codes.Internal, "stat password file failed: %v", err)
+		}
+	} else {
+		glog.Infof("password file %s is exist, and sha256 is same", passwdFilename)
+	}
+
+	return passwdFilename, nil
+}
+
+func getSecretCredential(bucket string, secrets map[string]string) (string, error) {
+	for k := range secrets {
+		if k != credentialID && k != credentialKey {
+			return "", fmt.Errorf("secret must contains %v or %v", credentialID, credentialKey)
+		}
+	}
+	sid := strings.TrimSpace(secrets[credentialID])
+	skey := strings.TrimSpace(secrets[credentialKey])
+	cosbucket := strings.TrimSpace(bucket)
+	return strings.Join([]string{cosbucket, sid, skey}, ":"), nil
+}
+
+func launcherCreateMountPoint(targetPath string) (bool, error) {
+	httpClient := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", SocketPath)
+			},
+		},
+	}
+
+	body := make(map[string]string)
+	body["stagingPath"] = targetPath
+	bodyJson, _ := json.Marshal(body)
+	response, err := httpClient.Post("http://unix/mount", "application/json", strings.NewReader(string(bodyJson)))
+	if err != nil {
+		return false, err
+	}
+
+	glog.Infof("http://unix/mount response is %v", response)
+
+	defer response.Body.Close()
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return false, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("the response of launcher mount is: %v", string(respBody))
+	}
+
+	var result map[string]string
+	if err := json.Unmarshal(respBody, &result); err != nil {
+		glog.Errorf("Unmarshal the response body of unix/mount failed. %v", err)
+		return false, err
+	}
+
+	isMounted, err := strconv.ParseBool(result["isMounted"])
+	if err != nil {
+		glog.Errorf("parse the value of `isMounted` from string to boolean failed. %v", err)
+		return false, err
+	}
+
+	return isMounted, nil
+}
