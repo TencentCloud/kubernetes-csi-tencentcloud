141fc8246211ee64e984384f57a6ae30d7f4a4b6
yujunwang <yujunwang@tencent.com>
2021-12-28 17:19:40 +0800

    bugfix: secret support only

--- driver/util/secret_util.go
+++ driver/util/secret_util.go
@@ -2,32 +2,10 @@ package util
 
 import (
 	"os"
-
-	"github.com/golang/glog"
-	"github.com/tencentcloud/kubernetes-csi-tencentcloud/driver/cloud"
 )
 
 func GetSercet() (secretID, secretKey, token string, isTokenUpdate bool) {
 	secretID = os.Getenv("TENCENTCLOUD_API_SECRET_ID")
 	secretKey = os.Getenv("TENCENTCLOUD_API_SECRET_KEY")
-
-	if secretID != "" && secretKey != "" {
-		return
-	}
-
-	var err error
-	if secretID == "" || secretKey == "" {
-		glog.Info("Get secretID or secretKey from env failed, will use cloud norm!")
-		secretID, secretKey, token, isTokenUpdate, err = GetNormTmpSecret()
-		if err != nil {
-			glog.Errorf("GetNormTmpSecret error %v", err)
-			return "", "", "", false
-		}
-	}
-
 	return
-}
-
-func GetNormTmpSecret() (string, string, string, bool, error) {
-	return cloud.GetNormCredentialInstance().GetCredential()
 }
