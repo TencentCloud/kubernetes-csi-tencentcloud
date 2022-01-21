6c5411a1eeda6ded17dfc6adf5d20fd09d5b6854
ginoczhu <ginoczhu@tencent.com>
2022-01-22 00:19:18 +0800

    sync Chinese tags

---
+++ driver/cbs/tags/config.go
@@ -0,0 +1,63 @@
+package tags
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	glog "k8s.io/klog"
+)
+
+const (
+	ConfigPath = "/etc/cbs-csi-config"
+)
+
+// GetConfigTags 获取 config 中的标签
+func GetConfigTags() (map[string]string, error) {
+	ConfigTags := make(map[string]string)
+
+	_, err := os.Stat(ConfigPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			glog.Infof("Create empty config %s", ConfigPath)
+			err := UpdateConfigTags(ConfigTags)
+			if err != nil {
+				glog.Errorf("Create empty cfsturboConfig %s failed, err: %v", ConfigPath, err)
+				return nil, err
+			}
+			return ConfigTags, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	fileContents, err := ioutil.ReadFile(ConfigPath)
+	if err != nil {
+		glog.Errorf("Read config failed, err: %v", err)
+		return nil, err
+	}
+
+	err = json.Unmarshal(fileContents, &ConfigTags)
+	if err != nil {
+		glog.Errorf("Unmarshal config failed, err: %v", err)
+		return nil, err
+	}
+
+	return ConfigTags, nil
+}
+
+// UpdateConfigTags 更新 config
+func UpdateConfigTags(ConfigTags map[string]string) error {
+	val, err := json.Marshal(ConfigTags)
+	if err != nil {
+		glog.Errorf("Marshal config failed, err: %v", err)
+		return err
+	}
+
+	err = ioutil.WriteFile(ConfigPath, val, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
